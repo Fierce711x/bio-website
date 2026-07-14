0a856946fac58287a71d538df5a5803046b37242
Fierec711x <Fierce711x@gmail.com>
2026-07-14 20:26:17 +0300

    created user Module

--- backEnd/src/app.module.ts
+++ backEnd/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './modules/UserModule/user.module';
 @Module({
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
