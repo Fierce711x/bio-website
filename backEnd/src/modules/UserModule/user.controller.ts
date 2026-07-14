7cb29dbbb6fbd4bf8cfcc744abd185f7bcf39ff4
Fierec711x <Fierce711x@gmail.com>
2026-07-14 22:18:54 +0300

    setup prisma and its module

--- backEnd/src/modules/UserModule/user.controller.ts
+++ backEnd/src/modules/UserModule/user.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { UserService } from './user.service';
+import { UserService } from './user.service.js';
 
 @Controller('user')
 export class UserController {
---
+++ backEnd/src/modules/prismaModule/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '../../generated/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(configService: ConfigService) {
+    const connectionString = configService.getOrThrow<string>('DATABASE_URL');
+    const adapter: PrismaPg = new PrismaPg({ connectionString });
+    super({ adapter });
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
